bc8080ed09e24dbf8c068bc63c036fc885a3d31a
liuarthur1 <57953071+liuarthur1@users.noreply.github.com>
2020-01-27 07:07:18 +0800

    first basic

---
+++ review.py
@@ -0,0 +1,12 @@
+# Read Files
+
+data=[]
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(count)
+
+print((data[0]))
