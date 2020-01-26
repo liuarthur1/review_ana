b0ff1de1572d96e6b2fe25f7bd94255435cf18d2
liuarthur1 <57953071+liuarthur1@users.noreply.github.com>
2020-01-27 07:24:44 +0800

    add some function

--- review.py
+++ review.py
@@ -9,4 +9,25 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(count)
 
-print((data[0]))
+print("File Read Finished. Total ", len(data), "lines")
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print("Average length is ", sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print("Over length 100: ", len(new))
+print(new[0])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print("Total ", len(good), "lines contains good")
+print(good[0])
